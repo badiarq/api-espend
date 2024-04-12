73d4d5a1e9b4c3ed422a730521389d73566118d3
Badr OUAHBI <68602055+badiarq@users.noreply.github.com>
2024-04-12 18:45:37 +0200

    spends fix

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use("/participants", require("./routes/participants"));
 app.use("/categories", require("./routes/categories"));
 app.use("/sub_categories", require("./routes/sub_categories"));
 app.use("/revenus", require("./routes/revenus"));
+app.use("/spends", require("./routes/spends"));
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
